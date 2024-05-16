f26d4e90e4738b9c535497899c80ddf88da4fee5
Brijithk <keranbrijith@gmail.com>
2024-05-15 22:32:19 -0700

    sample

---
+++ crispy.js
@@ -0,0 +1,102 @@
+function fun1()
+{
+	document.getElementById('section1').style.display="none";
+	document.getElementById('section2').style.display="none";
+	document.getElementById('section3').style.display="none";
+	document.getElementById('section4').style.display="none";
+	document.getElementById('section5').style.display="block";
+		 document.body.style.backgroundImage = "none"; // Remove the existing background image
+
+	 document.body.style.background = "linear-gradient(90deg,rgb(224, 14, 14),orange)"; // Remove the existing background image
+  
+}
+function fun2()
+{
+	document.getElementById('section1').style.display="block";
+	document.getElementById('section2').style.display="block";
+	document.getElementById('section3').style.display="block";
+	document.getElementById('section4').style.display="block";
+	document.getElementById('section5').style.display="none";
+	document.body.style.background = "linear-gradient(to bottom,rgb(224, 14, 14),orange,rgb(255, 81, 28),rgb(224, 14, 14))";
+	document.getElementById('section6').style.display="none";
+
+	
+
+}
+function fun3()
+{
+	document.getElementById('section1').style.display="none";
+	document.getElementById('section2').style.display="none";
+	document.getElementById('section3').style.display="none";
+	document.getElementById('section4').style.display="none";
+	document.getElementById('section5').style.display="none";
+	document.getElementById('section6').style.display="block";
+
+}
+function fun4()
+{
+	
+	document.getElementById('img5').src="bur1.jpg";
+
+}
+function fun5()
+{
+	
+	document.getElementById('img5').src="bur2.jpg";
+
+}
+function fun6()
+{
+	
+	document.getElementById('img5').src="bur3.jpg";
+
+}
+function fun7()
+{
+	
+	document.getElementById('img5').src="pizza.jpeg";                  
+
+}  
+function fun8()
+{
+	
+	document.getElementById('img5').src="noodles.jpeg";
+
+}
+function fun9()
+{
+	
+	document.getElementById('img5').src="meat.jpeg";
+
+}
+function fun10()
+{
+	
+	document.getElementById('img5').src="burgerking.jpg";
+
+}
+function fun11()
+{
+	
+	document.getElementById('img5').src="beef.jpeg";
+
+}
+function fun12()
+{
+	
+	document.getElementById('img5').src="bur2.jpg";
+
+}
+function fun13()
+{
+	
+	document.getElementById('img5').src="crs2.jpg";
+
+}
+function fun14()
+{
+	
+	document.getElementById('img5').src="bur1.jpg";
+
+}
+	
